2eb68cdc87dd565df75a8de01218a78b560d3ad2
ckartik <k5chopra@uwaterloo.ca>
2022-03-06 16:23:12 -0500

    Adds test with coordination.

--- hardhat/test/basic-test.js
+++ hardhat/test/basic-test.js
@@ -10,6 +10,17 @@ describe("Reputation Test", function () {
     await reputation.updateTrustRelations("test");
     expect(await reputation.getCID()).to.equal("test");
   });
+
+  it("Should coordinate reputation across addr1 and addr2", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    const Reputation = await ethers.getContractFactory("Reputation");
+    const reputation = await Reputation.deploy();
+    await reputation.deployed();
+    await reputation.updateTrustRelations("trust_1");
+    await reputation.connect(addr1).updateTrustRelations("QmTtB3qim1toPimVCzdb1DahLZaEmRw7MTaEkVj2tDyjza")
+    expect(await reputation.getCID()).to.equal("trust_1");
+    expect(await reputation.connect(addr1).getCID()).to.equal("QmTtB3qim1toPimVCzdb1DahLZaEmRw7MTaEkVj2tDyjza");
+  })
 });
 
 // TODO: Add test for multiple accounts in map.
