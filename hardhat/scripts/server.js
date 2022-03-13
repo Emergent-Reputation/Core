5197685ff497264a5547065a638bc2dcb4458486
ckartik <k5chopra@uwaterloo.ca>
2022-03-13 07:30:55 -0400

    Moves to all local ussage.

--- hardhat/scripts/server.js
+++ hardhat/scripts/server.js
@@ -28,8 +28,6 @@ async function upload_data(value){
 		codec: dagCBOR
 	})
 
-	console.log(payload.cid)
-
 	const { writer, out } = await CarWriter.create([payload.cid])
 	Readable.from(out).pipe(fs.createWriteStream('ipld-local.car'))
 
@@ -41,12 +39,14 @@ async function upload_data(value){
 }
 
 async function read_data(cid){
+	if (cid === "") {
+		return {out_edges: []}
+	}
 	const inStream = fs.createReadStream('ipld-local.car')
 	const reader = await CarReader.fromIterable(inStream)
 
 	const recovered_payload = await reader.get(cid)
 
-	console.log(dagCBOR.decode(recovered_payload.bytes))
 
 	return dagCBOR.decode(recovered_payload.bytes)
 }
@@ -57,21 +57,11 @@ upload_data({
 
 function local_deploy() {
 	const reputation =  ethers.getContractFactory("Reputation").then((Rep) =>  Rep.deploy())
-	// console.log("CONTRACT FACTORYY: %s", Reputation)
-    // const reputation = await Reputation.deploy();
-	console.log("CONTRACT DEPLOYED: %s", reputation)
-
-    // await reputation.deployed();
 
 	const accounts = ethers.getSigners();
-	console.log("I GET HREE")
 
 	return {"rep": reputation, "acc": accounts}
 }
-
-let obj = local_deploy()
-reputation = obj.rep
-accounts = obj.acc
 
 
 async function update_trusted_list(account, newTrustedAccount) {
@@ -83,166 +73,59 @@ async function update_trusted_list(account, newTrustedAccount) {
 		payload = await read_data(cid)
 	}
 	payload.out_edges.push(newTrustedAccount.address)
+	newCID = await upload_data(payload)
 
-	newCID = upload_data(payload)
+	await contract.connect(account).updateTrustRelations(newCID)
 
-	contract.connect(account).updateTrustRelations(newCID)
+	return newCID
 }
 
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 
-
-// function signAddress(public_key_to_sign) {
-// 	return web3.eth.accounts.sign(public_key_to_sign, secret_key);
-// }
-
-
-// function signAddress(public_key_to_sign) {
-// 	return web3.eth.accounts.sign(public_key_to_sign, secret_key);
-// }
-
-// /*{
-//         "identity": "0x0000",
-//         "in_signatures": ["baf0023e1", "baf0023e2", "baf0023e3",  "baf0023e4"], // 0x0000 has their pub key signed by key4.
-//         "in_pub_keys": ["key1", "key2", "key3", "Key4"],
-//         "out_pub_keys" ["k1", "k2"],
-//         "out_signatures": ["baf0023e1", "baf0023e2"]
-//     }*/
-// function update_in_signatures(prev_signatures, pk, signature) {
-// 	var data = {};
-// 	if (prev_signatures == '') {
-// 		data.identity = public_key;
-//         data.in_signatures = [];
-//         data.in_public_keys = [];
-//         data.out_public_keys = [];
-//        	data.out_signatures = [];
-// 	}
-// 	else {
-// 		data = JSON.parse(prev_signatures);
-// 	}
-// 	//assert(web3.eth.recover())
-// 	data.in_public_keys.push(pk);
-// 	data.in_signatures.push(signature);
-// 	updated_signatures = JSON.stringify(data);
-// 	return updated_signatures;
-// }
-
-// /*{
-//         "identity": "0x0000",
-//         "in_signatures": ["baf0023e1", "baf0023e2", "baf0023e3"],
-//         "in_pub_keys": ["key1", "key2", "key3"],
-//         "out_pub_keys" ["k1", "k2"],
-//         "out_signatures": ["baf0023e1", "baf0023e2", "baf0023e3"] // 0x0000 signs anothers pub k */
-// function update_out_signatures(prev_signatures, public_key_to_sign) {
-// 	var data = {};
-// 	if (prev_signatures == '') {
-// 		data.identity = public_key;
-//         data.in_signatures = [];
-//         data.in_public_keys = [];
-//         data.out_public_keys = [];
-//        	data.out_signatures = [];
-// 	}
-// 	else {
-// 		data = JSON.parse(prev_signatures);
-// 	}
-// 	data.out_public_keys.push(public_key_to_sign);
-// 	data.out_signatures.push(signAddress(public_key_to_sign).signature);
-// 	updated_signatures = JSON.stringify(data);
-// 	return updated_signatures;
-// }
-
-// // Using infura.io node for IPFS. Otherwise we run a daemon on our own computer.
-// const ipfs = create({ host: 'ipfs.infura.io', port: '5001', protocol: 'https'})
-
-// //Store data on IPFS.
-// async function get_cid(signatures) {
-//   const { cid } = await ipfs.add(signatures);
-//   console.log(cid);
-// }
-// // get_cid(update_out_signatures('', public_key));
-
-// //Update the cid of an address.
-// async function update_cid(cid) {
-//   reputation_contract.methods.updateTrustRelations(cid).send({from:public_key, gas:6000000});
-// }
-
-// //update_cid("QmdEmNGcqjMHoMzDrPriwaqCe8YbVAbf75pS5j25G8hiJE");
-
-// //Retrive data from IPFS.
-// async function retrieve_signatures(cid) {
-//   const stream = ipfs.cat(cid);
-//   let data = '';
-
-//   for await (const chunk of stream) {
-//   // chunks of data are returned as a Buffer, convert it back to a string
-//     data += chunk.toString();
-//   }
-//   console.log(data);
-//   data = JSON.parse(data);
-//   return data
-// }
-
-// //Get out_public_keys for ANY public address.
-// async function get_out_public_keys(pk) {
-//   const cid = reputation_contract.methods.getCIDFor(pk).call({from:public_key, gas:5000000});
-//   data = retrieve_signatures(cid);
-//   //console.log("rectrieved cid", cid);
-//   return data.out_public_keys;	
-// }
-
-// retrieve_signatures("QmdEmNGcqjMHoMzDrPriwaqCe8YbVAbf75pS5j25G8hiJE");
-//Check if two addresses are connected within a certain distance.
-// function find_connection(pk, distance) {
-//   const queue = [[public_key,0]];
-//   const result = [];
-//   const visited = {};
-//   visited[public_key] = true;
-//   let currentVertex;
-//   let level;
-//   while (queue.length) {
-//     next = queue.shift();
-//     currentVertex = next[0];
-//     level = next[1];
-//     result.push(currentVertex);var keys = get_out_public_keys(currentVertex);
-//     var keys = get_keys(currentVertex);
-//     for(var i = 0; i < keys.length; i++) {
-//       if (keys[i] == pk && level + 1 <= distance) {
-//       	return true;
-//       }
-//       if (!visited[keys[i]]) {
-//       	if (level + 1 > distance) {
-//       	  return false;
-//       	}
-//         visited[keys[i]] = true;
-//         queue.push([keys[i], level+1]);
-//       }
-//     };
-//   }
-//   return false;
-// }
-
-// retrieve_signatures("QmaMuUwaS6bqEkgEJeHSvYd4258654qwTixXwQTsi3QPH2");
-//console.log(update_cid("hi"));
-//content();
-
-// GET ACCOUNTS
+let obj = local_deploy()
+let reputation = obj.rep
+let eth_accounts = obj.acc
 
 // 
+app.get('/accounts', (req, res) => {
+	eth_accounts.then((accounts) => res.send(accounts.slice(10).map((v,idx) => Object({ref_id: idx, address: v.address}))))
+})
 
+app.get('/accounts/trust-relations', async (req, res) => {
+	const accounts = await eth_accounts
+	const contract = await reputation
+	const cid = await contract.connect(accounts[req.body.ref_id]).getCID()
+	const payload = await read_data(cid)
+	res.send(payload)
+	// accounts.then((accounts) => 
+	// 	reputation.then((contract) => {
+	// 		res.send(await read_data(await contract.connect(accounts[refID]).getCID()))
+	// 	}
+	// ))
+})
 
-console.log("HERE LOOK AT MEEEEE: %s", reputation)
+app.post('/accounts/trust-relations', async (req, res) => {
+	const accounts = await eth_accounts
+	const contract = await reputation
+	const newCID = await update_trusted_list(accounts[req.body.ref_id], accounts[req.body.account_to_add])
+	const newPayload = await read_data(newCID)
+	res.send(Object({
+		new_cid: newCID,
+		payload: newPayload
+	}))
+})
+
 app.get('/', (req, res) => {
-	retrieve_signatures("QmaMuUwaS6bqEkgEJeHSvYd4258654qwTixXwQTsi3QPH2").then((data) =>
-	res.send(data.toString())
-	)
+	res.send("Hello World!")
 })
 
 app.get('/contract', (req, res) => {
 	reputation.then((contract) => 	{
-		accounts.then(
+		eth_accounts.then(
 			(accounts) =>  contract.getCIDFor(accounts[0].address).then((resp) => res.send(resp))
 		)
 	}
@@ -252,7 +135,7 @@ app.get('/contract', (req, res) => {
 app.get('/contract-add-cid/:cid', (req, res) => {
 	const cid = req.params.cid
 	reputation.then((contract) => 	{
-		accounts.then(
+		eth_accounts.then(
 			(accounts) =>  contract.connect(accounts[0]).updateTrustRelations(cid.toString()).then(contract.getCID().then((resp)=> res.send(resp)))
 		)
 	}
@@ -260,7 +143,7 @@ app.get('/contract-add-cid/:cid', (req, res) => {
 })
 
 app.get('/accounts', (req, res) => {
-	accounts.then(
+	eth_accounts.then(
 		(accounts) => res.send(accounts[0].address)
 	)
 })
