a27a3fbab412da26199f1a58f0c4a9cf4c1b41fd
ckartik <k5chopra@uwaterloo.ca>
2022-04-24 00:14:14 +0200

    Fuck it - commit the keys.

---
+++ hardhat/contracts/Stream.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { 
+    ISuperfluid 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import { 
+    IConstantFlowAgreementV1 
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    CFAv1Library
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+
+contract CFALibraryMock {
+
+    using CFAv1Library for CFAv1Library.InitData;
+    
+    //initialize cfaV1 variable
+    CFAv1Library.InitData public cfaV1;
+    
+    constructor(
+        ISuperfluid host
+    ) {
+    
+    //initialize InitData struct, and set equal to cfaV1
+    cfaV1 = CFAv1Library.InitData(
+        host,
+        //here, we are deriving the address of the CFA using the host contract
+        IConstantFlowAgreementV1(
+            address(host.getAgreementClass(
+                    keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1")
+                ))
+            )
+        );
+    }
+    
+    function(uint stakedAmount)
+    //your contract code here...
+}
