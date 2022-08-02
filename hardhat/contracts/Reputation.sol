25969eecc5c5f85ad821ab8aac56636930294d55
jialinli <jialinmli98@gmail.com>
2022-08-01 23:48:36 -0700

    update contract and tests

--- hardhat/contracts/Reputation.sol
+++ hardhat/contracts/Reputation.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-
 import "hardhat/console.sol";
 
 contract Reputation {
@@ -22,5 +21,14 @@ contract Reputation {
         console.log("SMART CONTRACT: SENDER %s ", msg.sender);
         console.log("SMART CONTRACT: SENDER CID %s ", CID);
         Trusted[msg.sender] = CID;
+    }
+
+    function removeTrustRelations() public {
+        if (bytes(Trusted[msg.sender]).length == 0) {
+            console.log("Sender %s has no data stored in the system", msg.sender);
+            return;
+        }
+        delete Trusted[msg.sender];
+        console.log("Sender %s has been removed from the system", msg.sender);
     }
 }
--- hardhat/test/basic-test.js
+++ hardhat/test/basic-test.js
@@ -10,6 +10,9 @@ describe("Basic Deployment & Functionality", function () {
 
     await reputation.updateTrustRelations("test");
     expect(await reputation.getCID()).to.equal("test");
+
+    await reputation.removeTrustRelations();
+    expect(await reputation.getCID()).to.equal("");
   });
 
   it("Should coordinate reputation across addr1 and addr2", async function () {
@@ -18,9 +21,20 @@ describe("Basic Deployment & Functionality", function () {
     const reputation = await Reputation.deploy();
     await reputation.deployed();
     await reputation.updateTrustRelations("trust_1");
+    await reputation.connect(addr2).updateTrustRelations("trust_2")
     await reputation.connect(addr1).updateTrustRelations("QmTtB3qim1toPimVCzdb1DahLZaEmRw7MTaEkVj2tDyjza")
     expect(await reputation.getCIDFor(owner.address)).to.equal("trust_1");
     expect(await reputation.connect(addr1).getCIDFor(addr1.address)).to.equal("QmTtB3qim1toPimVCzdb1DahLZaEmRw7MTaEkVj2tDyjza");
+    expect(await reputation.connect(addr2).getCIDFor(addr2.address)).to.equal("trust_2");
+    
+
+  })
+
+  it("Should delete addr1 in mapping when requested", async function () {
+    const Reputation = await ethers.getContractFactory("Reputation");
+    const reputation = await Reputation.deploy();
+    await reputation.deployed();
+
   })
 });
 
