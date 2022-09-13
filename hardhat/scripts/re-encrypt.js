5c6abb507f6b7f042754eb3ed79c34014a8a1390
ckartik <k5chopra@uwaterloo.ca>
2022-09-13 00:41:30 -0400

    adds untested implementation of addTrustRelation function.

--- hardhat/scripts/re-encrypt.js
+++ hardhat/scripts/re-encrypt.js
@@ -1,12 +1,39 @@
 const { ethers } = require("hardhat");
 
 
+fs = require('fs')
+const { Readable } = require('stream')
+const { CID } = require('multiformats/cid')
+const Block = require('multiformats/block')
+
+const { sha256 } = require('multiformats/hashes/sha2')
+const dagCBOR = require('@ipld/dag-cbor')
+const { curve } = require('@futuretense/curve25519-elliptic');
+
+
+// CAR utilities, see https://github.com/ipld/js-car for more info
+const { CarWriter } = require( '@ipld/car/writer')
+const { CarReader } = require( '@ipld/car/reader');
+const { PRE } = require("@futuretense/proxy-reencryption");
+
+
+/* 
+  We can leverag Tags to create trust-relations that you want or don't want exposed.
+  We can repeat add L1 Tier Relations to Lower Tiers.
+
+  
+*/
 
 class EmergentReputation {
   static async create(secretKey, contractAddress) {
     return new EmergentReputation(secretKey, await ethers.getContractAt("Reputation", contractAddress));
   }
-  
+  constructor(secretKey, contract) {
+    this.contract = contract
+    this.wallet = new ethers.Wallet(secretKey, ethers.provider)
+    this.privateKeyScalar = curve.scalarFromBuffer(Buffer.from(secretKey.substring(2), 'hex'))
+    this.PRECore = new PRE(this.privateKeyScalar.toBuffer(), curve)
+}
   // Try storing data as follows:
   /*
     {
@@ -14,6 +41,23 @@ class EmergentReputation {
       payload: <bytes> -> Only decode if unencrypted, or decrypt and decode.
     }
   */
+  static async upload_data(value){
+      const payload = await Block.encode({
+        value: value,
+        hasher: sha256,
+        codec: dagCBOR
+      })
+    
+      const { writer, out } = await CarWriter.create([payload.cid])
+      Readable.from(out).pipe(fs.createWriteStream('ipld-local.car'))
+    
+      writer.put(payload)
+    
+      await writer.close()
+    
+      return payload.cid
+    }
+
   static async read_data(cid){
     if (cid === "") {
       return {out_edges: []}
@@ -29,23 +73,64 @@ class EmergentReputation {
     return dagCBOR.decode(recovered_payload.bytes)
   }
   
-  constructor(secretKey, contract) {
-      this.contract = contract
-      this.wallet = new ethers.Wallet(secretKey, ethers.provider)
-  }
+  
 
   async getAddress() {
     return await this.contract.connect(this.wallet).getAddress();
   }
 
-  async addTrustRelation() {
+  async addEncryptedTrustRelation() {
+
+  }
+  
+  async encrypt(value, tag) {
+    return await this.PRECore.selfEncrypt(value, tag);
+  }
+
+  async decrypt(value, tag) {
+    return await this.PRECore.selfDecrypt(value, tag);
+  }
+
+  async decrypto(value, tag){}
+
+  // addTrustRelation adds the [newTrustedAddress] to the adjacency list index by the ownwer node.
+  // The securityLevel dictates the minimum level of security access needed to access the list from [T0, T1, T2, T3].
+  // T3 being the most secured data, T0 being unencrypted.
+  //
+  // We utilize a model similar to BLP (Bell–LaPadula model) model constrainted to a view only prespective for re-encrpytion keys.
+  async addTrustRelation(newTrustedAddress, securityLevel) {
+    
+    // Retrive data from IPLD or initialize.
     const cid = await contract.connect(this.Wallet).getCID()
     if (cid === ""){
-      payload = {out_edges: []}
+      payload = {T0: [], T1: [], T2: [], T3: []};
     } else {
-      payload = await EmergentReputation.read_data(cid)
+      payload = await EmergentReputation.read_data(cid);
     }
-    // What to do with the payload -> Likely want to alter and post.
+
+    // Add new data to payload.
+    switch (securityLevel) {
+      case "T0":
+        payload.T0.push(newTrustedAddress);
+        // Fall-through
+      case "T1":
+        payload.T1.push(this.encrypt(newTrustedAddress, "T1"));
+        // Fall-through
+      case "T2":
+        payload.T2.push(this.encrypt(newTrustedAddress, "T2"));
+        // Fall-through
+      case "T3":
+        payload.T3.push(this.encrypt(newTrustedAddress, "T3"));
+        break;
+      default:
+        throw "Security Level passed cannot be understood";
+    }
+
+    // Update persistent data stores
+    const newCID = await upload_data(payload);
+    await this.contract.connect(this.wallet).updateTrustRelations(newCID.toString());
+
+    return newCID
   }
 
   async removeTrustRelation() {
@@ -74,5 +159,13 @@ console.log(aliceWallet.address);
 const ERAdapter = await EmergentReputation.create(aliceWallet.privateKey, reputation.address);
 
 console.log( await ERAdapter.getAddress());
+const payload = await ERAdapter.encrypt('kartik', 'name');
+
+const cid =  await EmergentReputation.upload_data(payload)
+console.log(cid)
+const newPayload = await EmergentReputation.read_data(cid)
+console.log( newPayload)
+const plaintext = await ERAdapter.decrypt(newPayload, newPayload.tag);
+console.log(plaintext.toString());
 
 })();
