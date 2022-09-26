e513c06d5b708f6da47939eacf5b7488865ea6ee
ckartik <k5chopra@uwaterloo.ca>
2022-09-25 23:23:15 -0400

    Backend working effectively.

--- hardhat/scripts/demo-backend/demo.js
+++ hardhat/scripts/demo-backend/demo.js
@@ -9,11 +9,12 @@ const app = express()
 const  {EmergentReputation, SecurityLevels} = require('../../../sdk/emergent-reputation');
 app.use(express.json())
 const contractAddress = "0xEa4Df49aEe4bB81EcDE7dB26dD638F2B6DfCC961"
-// var ERCustomer;
-// async function init() {
-//   ERCustomer = await await EmergentReputation.create("0e10373c761cbe50eafe9798cb8df4ed9edeb13c1396684daa0f8eefd6022abc", contractAddress)
-// }
 
+app.get('/address', async (req,res) => {
+  const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
+
+  res.send(ERLocksmith.getAddress())
+})
 
 app.get('/relation', async (req, res) => {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
@@ -28,7 +29,7 @@ app.post('/relation', async (req, res) => {
 
 app.get('/customers', async (req, res) => {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
-  const list = await ERLocksmith.getCustomerList();
+  const list = await ERLocksmith.getCustomers();
 
   res.send(list)
 })
@@ -39,7 +40,6 @@ app.post('/request-decrypt', async (req, res) => {
 
   res.send(tx)
 })
-
 
 app.post('/approve-request', async (req, res) => {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
