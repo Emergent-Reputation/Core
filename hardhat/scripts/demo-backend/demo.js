8341d436bb6491c4be56708718810c10716ba29e
ckartik <k5chopra@uwaterloo.ca>
2022-09-28 03:15:36 -0400

    WIP 2

--- hardhat/scripts/demo-backend/demo.js
+++ hardhat/scripts/demo-backend/demo.js
@@ -1,66 +1,101 @@
 const express = require('express')
+var cors = require('cors');
+
 const app = express()
 
 
 const  {EmergentReputation, SecurityLevels} = require('../../../sdk/emergent-reputation');
 
-// NOTE: This contract address needs to contains CIDs that exist in an accessible IPFS/IPLD datastore.
-const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+// NOTE: This contract address needs to contains CIDs that exist in an accessible IPFS/IPLD
+const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
-
+app.use(cors())
 app.use(express.json())
 
 app.get('/address', async (req,res) => {
-  const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
-
-  res.send(ERLocksmith.getAddress())
+  
+  try {
+    const ERLocksmith = await EmergentReputation.create(req.query.key, contractAddress)
+    res.json({ address: ERLocksmith.getAddress()})
+  } catch {
+    res.status(500).json({message: "Unable to process"})
+  }
 })
 
 app.get('/relation', async (req, res) => {
-  const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
-  res.send( await ERLocksmith.getTrustRelations(ERLocksmith.getAddress()))
+  try {
+    const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
+    res.send( { relation: await ERLocksmith.getTrustRelations(ERLocksmith.getAddress())})
+  } catch {
+    res.status(500).json({message: "Unable to process"})
+  }
+  
 })
 
 app.post('/relation', async (req, res) => {
+  try {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
   const cid = await ERLocksmith.addTrustRelation(req.body.value, req.body.tier)
-  res.send(cid)
+  res.send({cid: cid.toString()})
+} catch {
+  res.status(500).json({message: "Unable to process"})
+}
 })
 
 app.get('/customers', async (req, res) => {
+  try {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
   const list = await ERLocksmith.getCustomers();
 
-  res.send(list)
+  res.send({customers:list})
+} catch {
+  res.status(500).json({message: "Unable to process"})
+}
 })
 
 app.post('/request-decrypt', async (req, res) => {
+  try {
   const ERCustomer = await EmergentReputation.create(req.body.key, contractAddress)
   const tx = await ERCustomer.requestDecryption(req.body.locksmith, req.body.tier);
 
-  res.send(tx)
+  res.send({tx:tx})
+} catch {
+  res.status(500).json({message: "Unable to process"})
+}
 })
 
 app.post('/approve-request', async (req, res) => {
+  try {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
   const tx = await ERLocksmith.approveRequest(req.body.customer);
 
-  res.send(tx)
+  res.send({tx:tx})
+} catch {
+  res.status(500).json({message: "Unable to process"})
+}
 })
 
 
 app.post('/get-decrypted-relations', async (req, res) => {
+  try {
   const ERCustomer = await EmergentReputation.create(req.body.key, contractAddress)
   const list = await ERCustomer.getDecryptedTrustRelation(req.body.locksmith, req.body.tier);
 
-  res.send(list)
+  res.send({list:list})
+} catch {
+  res.status(500).json({message: "Unable to process"})
+}
 })
 
 app.post('/clear-funds', async (req, res) => {
+  try {
   const ERLocksmith = await EmergentReputation.create(req.body.key, contractAddress)
   const reciept = await ERLocksmith.clearFunds(req.body.customer);
 
-  res.send(reciept);
+  res.send({reciept:reciept});
+  } catch {
+    res.status(500).json({message: "Unable to process"})
+  }
 })
 
 app.listen(8080)
